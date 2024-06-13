f7df1dd75add7a52b29d92df567748216b503256
Kudzeri <kudzeri@gmail.com>
2024-06-13 15:41:06 +0500

    init

--- main.go
+++ main.go
@@ -3,21 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/Kudzeri/Boot.dev-pokedex-go/controller"
 	"os"
 )
 
 func main() {
-	sc := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("pokedex > ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
 
-		input, err := sc.ReadString('\n')
-		if err != nil {
-			fmt.Println("Reading error:", err)
-			return
-		}
-
-		controller.ExecuteCommand(input)
-	}
+	fmt.Println("pokedex> ", text)
 }
--- model/commands.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type CliCommand struct {
-	Name        string
-	Description string
-	Callback    func() error
-}
-
-var CliCommandList map[string]CliCommand
