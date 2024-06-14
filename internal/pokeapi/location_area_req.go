71d22097890f38dc4da82edcc7a163cfab301b8b
Kudzeri <kudzeri@gmail.com>
2024-06-14 19:35:34 +0500

    fix bug with location list

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (c Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
-	endpoint := "/location-area"
+	endpoint := "/location-area/?offset=0&limit=20"
 	fullURL := baseURL + endpoint
 
 	if pageURL != nil {
